4058e99646f36aff97e88d4b2bf0b2a01ecbdef1
rodrigo.salado <rodrigo.salado@movivendor.com>
2020-10-06 23:21:57 -0500

    Se depliegan de forma ordenada los verticles desde ApiVerticle

--- api/src/main/java/zxcv/jvmmx/api/ApiVerticle.java
+++ api/src/main/java/zxcv/jvmmx/api/ApiVerticle.java
@@ -1,5 +1,6 @@
 package zxcv.jvmmx.api;
 
+import io.reactivex.rxjava3.core.Single;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import org.slf4j.Logger;
@@ -11,12 +12,26 @@ public class ApiVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
+    Single.just(true)
+      .flatMap(n -> deploy("maven:zxcv.jvmmx:webservice:1.0::service"))
+      .flatMap(n -> deploy("maven:zxcv.jvmmx:database:1.0::service"))
+      .flatMap(n -> deploy("maven:zxcv.jvmmx:email:1.0::service"))
+      .doOnSuccess(n -> log.info("+++ ApiVerticle inicido"))
+      .subscribe(
+        n -> startPromise.complete(),
+        e -> startPromise.fail(e)
+      );
+  }
 
-    vertx.deployVerticle("maven:zxcv.jvmmx:webservice:1.0::service");
-    vertx.deployVerticle("maven:zxcv.jvmmx:database:1.0::service");
-    vertx.deployVerticle("maven:zxcv.jvmmx:email:1.0::service");
-
-    log.info("+++ ApiVerticle inicido");
-    startPromise.complete();
+  private Single<String> deploy(String verticle) {
+    return Single.create(emitter -> {
+      vertx.deployVerticle(verticle, ar -> {
+        if(ar.succeeded()) {
+          emitter.onSuccess(ar.result());
+        } else {
+          emitter.onError(ar.cause());
+        }
+      });
+    });
   }
 }
